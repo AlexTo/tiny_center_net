81b99c803af6c40802c48cc53a4cbe2d0ac3a136
Alex To <tonhudung@gmail.com>
2020-07-11 23:57:23 +1000

    wip

--- src/merge_bbox_result_with_ann_file.py
+++ src/merge_bbox_result_with_ann_file.py
@@ -1,14 +1,33 @@
 import argparse
+import json
 
 
 def init_args():
     parser = argparse.ArgumentParser()
     parser.add_argument("--result_file", type=str,
+                        default="exp/ctdet_tiny/default/FreeAnchorNms0.5.json",
+                        required=False)
+    parser.add_argument("--annotation_file", type=str,
                         default="data/tiny_set/erase_with_uncertain_dataset/annotations/tiny_set_test_nobox.json",
                         required=False)
-    parser.add_argument("--dst_dir", type=str,
-                        default="data/tiny_set/erase_with_uncertain_dataset/annotations/", required=False)
-    parser.add_argument("--w", type=int, default=512, required=False)
-    parser.add_argument("--h", type=int, default=640, required=False)
+
+    parser.add_argument("--dst_file", type=str,
+                        default="data/tiny_set/erase_with_uncertain_dataset/annotations/tiny_set_test_yesbox.json",
+                        required=False)
 
     return parser.parse_args()
+
+
+if __name__ == '__main__':
+    args = init_args()
+
+    with open(args.result_file, 'r') as f:
+        result = json.load(f)
+
+    with open(args.annotation_file, 'r') as f:
+        annotation = json.load(f)
+
+    annotation['annotations'] = result
+
+    with open(args.dst_file, 'w') as f:
+        json.dump(annotation, f)
